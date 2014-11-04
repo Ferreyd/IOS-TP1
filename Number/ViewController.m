92923cd2598e5c9dcb8bd0cf69655c6cf4e01c20
MBDS <mbds@Mac-mini-de-MBDS-9.local>
2014-11-04 10:45:29 +0100

    Initial Commit

---
+++ Number/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Number
+//
+//  Created by MBDS on 04/11/2014.
+//  Copyright (c) 2014 MBDS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
